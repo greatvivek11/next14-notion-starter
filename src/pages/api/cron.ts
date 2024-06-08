4ea905b931c6d971bd6630f865a73184145ab66b
greatvivek11 <greatvivek11@proton.me>
2024-06-08 20:38:12 +0530

    fixed - cron job API

---
+++ src/pages/api/cron.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+    request: NextApiRequest,
+    response: NextApiResponse,
+) {
+    const authHeader = request.headers['authorization'];
+    if (
+        !process.env.CRON_SECRET ||
+        authHeader !== `Bearer ${process.env.CRON_SECRET}`
+    ) {
+        return response.status(401).json({ success: false });
+    }
+
+
+    await fetch(`${process.env.CRON_URL}`, {
+        method: 'POST',
+    })
+
+    response.status(200).json({ success: true });
+}
